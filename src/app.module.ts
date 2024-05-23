d1de1ceb7b83a775ea0ab0cbb4b3d3e3e6e251e7
Volodymyr Nerovnia <nerv@i.ua>
2024-05-23 12:53:50 +0300

    Added controllers to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { PostsController } from './posts/posts.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, AuthController, PostsController],
   providers: [AppService],
 })
 export class AppModule {}
