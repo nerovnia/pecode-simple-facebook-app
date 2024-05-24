001095c74b374d546487386a33076b7548fb3e94
Volodymyr Nerovnia <nerv@i.ua>
2024-05-24 14:24:58 +0300

    Refactored code in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 import { PostsController } from './posts/posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/database.config';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
+
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseConfig)],
-  controllers: [AppController, AuthController, PostsController],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfig),
+    UsersModule,
+    AuthModule,
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [AppController, PostsController],
   providers: [AppService],
 })
 export class AppModule {}
