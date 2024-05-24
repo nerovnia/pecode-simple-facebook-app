8493c8009a9a1e41c3bea78acbadeb235a4c5af3
Volodymyr Nerovnia <nerv@i.ua>
2024-05-24 14:17:28 +0300

    Ctreated users module and service

---
+++ src/users/users.service.ts
@@ -0,0 +1,17 @@
+
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  findOne(id: number): Promise<User | null> {
+    return this.usersRepository.findOneBy({ id });
+  }
+}
