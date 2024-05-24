d29eecbf84669ba285ef3cf81fe09479fe390a47
Volodymyr Nerovnia <nerv@i.ua>
2024-05-24 14:22:50 +0300

    Updated auth controller: get user data and save it into the database

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,37 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Body } from '@nestjs/common';
+//import crypto from 'crypto';
+import { createHash } from 'node:crypto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Get('user')
   getUser(): string {
     return 'Get User';
   }
 
   @Post('register')
-  register(): string {
-    return 'Post register';
+  @HttpCode(201)
+  async register(
+    @Body('name') fullName: string,
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const hash = createHash('sha256').update(password).digest('hex');
+
+    const res = this.authService.create({
+      fullName,
+      email,
+      password: hash,
+    });
+    return JSON.stringify(res);
   }
 
   @Post('login')
-  login(): string {
+  @HttpCode(200)
+  async login() {
     return 'Post login';
   }
 }
