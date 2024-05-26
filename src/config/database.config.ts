e6d7afa05a3d8e5594d7f56fb0ab531e998b23d4
Volodymyr Nerovnia <nerv@i.ua>
2024-05-26 10:14:29 +0300

    Added default parameters in databasee.config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,11 +6,12 @@ dotenv.config();
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: Number.parseInt(process.env.DATABASE_PORT),
-  username: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: Number.parseInt(process.env.DATABASE_PORT) || 5432,
+  username: process.env.DATABASE_USER || 'postgres',
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_DBNAME,
   entities: [User, Post],
-  synchronize: true, // Set this to false in production
+  synchronize: !global.isProduction,
+  logging: !global.isProduction,
 };
