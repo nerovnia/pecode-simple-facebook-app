b238109ff25cb7a89a0e2263d927b1eec405a3a2
Volodymyr Nerovnia <nerv@i.ua>
2024-05-27 18:43:28 +0300

    Enhanced main.ts, make app port changeble

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import './global';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { appConfig } from './config/app.default.config';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.APP_PORT || 3000);
+  await app.listen(process.env.APP_PORT || appConfig.main.port);
 }
 bootstrap();
