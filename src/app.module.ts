4b532f0d66ac594b9237e565c25d6897f8b11f91
Volodymyr Nerovnia <nerv@i.ua>
2024-05-27 18:40:52 +0300

    Remove PostsController and added AuthService and JwtModule to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
-import { PostsController } from './modules/posts/posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/database.config';
 import { UsersModule } from './modules/users/users.module';
 import { User } from './modules/users/user.entity';
+import { PostsModule } from './modules/posts/posts.module';
+import { JwtModule } from '@nestjs/jwt';
+import { appConfig } from './config/app.default.config';
+import 'dotenv/config';
+import { AuthService } from './modules/auth/auth.service';
+
+//import { UserRepository } from './modules/auth/';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(databaseConfig),
     UsersModule,
     AuthModule,
+    PostsModule,
     TypeOrmModule.forFeature([User]),
+    PostsModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXP_TIME || appConfig.auth.jwtExpTime },
+    }),
   ],
-  controllers: [AppController, PostsController],
-  providers: [AppService],
+  controllers: [AppController],
+  providers: [AppService, AuthService],
+  exports: [AuthService],
 })
 export class AppModule {}
